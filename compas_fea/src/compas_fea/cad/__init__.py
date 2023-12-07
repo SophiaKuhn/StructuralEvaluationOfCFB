5a2a9a8ebf9f83034787cdf7a24c8fd238e5293a
Kuhn  Sophia <98028544+SophiaKuhn@users.noreply.github.com>
2023-12-07 17:33:49 +0100

    added compas_fea files

---
+++ compas_fea/src/compas_fea/cad/__init__.py
@@ -0,0 +1,92 @@
+"""
+
+********************************************************************************
+cad
+********************************************************************************
+
+
+
+
+.. currentmodule:: compas_fea.cad
+
+The compas_fea package for struceng lib supports Rhino in the frontend.
+
+
+Rhino
+=====
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    add_element_set
+    add_node_set
+    add_nodes_elements_from_layers
+    add_sets_from_layers
+    add_tets_from_mesh
+    discretise_mesh
+    mesh_extrude
+    network_from_lines
+    ordered_network
+    plot_reaction_forces
+    plot_concentrated_forces
+    plot_mode_shapes
+    plot_volmesh
+    plot_axes
+    plot_data
+    plot_principal_stresses
+    plot_voxels
+    weld_meshes_from_layer
+
+"""
+
+# Author(s): Compas/Compas FEA Team, Marius  Weber (ETHZ, HSLU T&A)
+
+from __future__ import absolute_import
+
+import compas
+
+if compas.RHINO:
+    from .rhino import (  # noqa: F401
+        add_element_set,
+        add_node_set,
+        add_nodes_elements_from_layers,
+        add_sets_from_layers,
+        add_tets_from_mesh,
+        discretise_mesh,
+        mesh_extrude,
+        network_from_lines,
+        ordered_network,
+        plot_reaction_forces,
+        plot_concentrated_forces,
+        plot_mode_shapes,
+        plot_volmesh,
+        plot_axes,
+        plot_data,
+        plot_principal_stresses,
+        plot_voxels,
+        weld_meshes_from_layer,
+    )
+    __all__ = [
+        'add_element_set',
+        'add_node_set',
+        'add_nodes_elements_from_layers',
+        'add_sets_from_layers',
+        'add_tets_from_mesh',
+        'discretise_mesh',
+        'mesh_extrude',
+        'network_from_lines',
+        'ordered_network',
+        'plot_reaction_forces',
+        'plot_concentrated_forces',
+        'plot_mode_shapes',
+        'plot_volmesh',
+        'plot_axes',
+        'plot_data',
+        'plot_principal_stresses',
+        'plot_voxels',
+        'weld_meshes_from_layer',
+    ]
+
+else:
+    __all__ = []
---
+++ compas_fea/src/compas_fea/fea/__init__.py
@@ -0,0 +1,55 @@
+"""
+********************************************************************************
+fea
+********************************************************************************
+
+.. currentmodule:: compas_fea.fea
+
+The compas_fea package with struceng lib supports Ansys as analysis backends.
+
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+
+    Writer
+
+
+Backends
+========
+
+
+ansys
+-----
+
+.. currentmodule:: compas_fea.fea.ansys
+
+.. autosummary::
+    :toctree: generated/
+
+    input_generate
+    make_command_file_static
+    make_command_file_modal
+    make_command_file_harmonic
+    ansys_launch_process
+    ansys_launch_process_extract
+    delete_result_files
+    extract_rst_data
+    write_results_from_rst
+    load_to_results
+
+
+"""
+# Author(s): Compas/Compas FEA Team, Marius  Weber (ETHZ, HSLU T&A)
+
+from __future__ import absolute_import
+
+from .writer import Writer
+
+
+
+__all__ = [
+    'Writer'
+]
